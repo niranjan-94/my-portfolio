16c2a772071c541a839c776674c82e2c5eb017c3
niranjan-94 <niranjan.meher.94@gmail.com>
2022-09-07 09:52:06 +0530

    first-commit

---
+++ src/data.js
@@ -0,0 +1,44 @@
+export const projects = [
+  {
+    title: "Sell My Cell Website",
+    subtitle: "React JS",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+    image: "./project-1.gif",
+    link: "/",
+  },
+  {
+    title: "React Todo App",
+    subtitle: "React JS",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+    image: "./project-2.gif",
+    link: "/",
+  },
+  {
+    title: "My Resto: Restaurant Website",
+    subtitle: "JavaScript, CSS, HTML",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+    image: "./project-3.gif",
+    link: "/",
+  },
+  {
+    title: "Tribute Page for Elon Musk",
+    subtitle: "HTML & CSS",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+    image: "./project-4.gif",
+    link: "/",
+  },
+];
+export const skills = [
+  "React JS",
+  "Next JS",
+  "Graph QL",
+  "JavaScript",
+  "Tailwind CSS",
+  "Bootstrap",
+  "CSS",
+  "HTML",
+];
